8c1080c53ebd39f0a6fc1d0a68cb22ed6a1d2f6f
CamaroBoss <150494177+CamaroBoss@users.noreply.github.com>
2024-12-17 13:45:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,11 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing
-    nigger: "owen",
+    mike: "lunini",
     brayden: "gray",
+    ethan:"28",
+    ojf0103: "1234";
+  
     interstellar: "password",
   },
 };
